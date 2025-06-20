5b8c828e0a5a52e0de07e49ea42f7965476c0096
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-20 00:51:34 +0000

    [automatic commit] 2025-06-20 00:51:34 UTC 115.131.91.233

--- part1/question5.sql
+++ part1/question5.sql
@@ -16,4 +16,4 @@ INSERT INTO Dogs (name, size, owner_id) VALUES
 
 -- Insert: Five walk request:
 INSERT INTO WalkRequests (dog_id, requested_time, duration, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'MAX'), )
+((SELECT dog_id FROM Dogs WHERE name = 'MAX'), '2025-06-10 08:00:00', 20, 'Lakesview', )
