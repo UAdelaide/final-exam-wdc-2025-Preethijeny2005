a25513ee8d9623ff260128919a6e93094d04044e
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-20 00:53:16 +0000

    [automatic commit] 2025-06-20 00:53:16 UTC 115.131.91.233

--- part1/question5.sql
+++ part1/question5.sql
@@ -17,7 +17,7 @@ INSERT INTO Dogs (name, size, owner_id) VALUES
 -- Insert: Five walk request:
 INSERT INTO WalkRequests (dog_id, requested_time, duration, location, status) VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 20, 'Parklands', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-07-10 08:00:00', 30, 'Pinkssview', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Maxine'), '2025-08-10 08:00:00', 45, 'Lakesview', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Bellarina'), '2025-09-10 08:00:00', 20, 'Lakesview', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Basil'), '2025-10-10 08:00:00', 20, 'Lakesview', 'open');
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-07-10 08:00:00', 30, 'Beachside Ave', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Maxine'), '2025-08-10 08:00:00', 45, 'Pinksview', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bellarina'), '2025-09-10 08:00:00', 20, 'Testsview', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Basil'), '2025-10-10 08:00:00', 20, 'View', 'open');
