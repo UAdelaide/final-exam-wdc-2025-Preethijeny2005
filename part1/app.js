7a47d52e75397bbb1e3968744d7d5857063aa46a
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-20 06:22:50 +0000

    [automatic commit] 2025-06-20 06:22:50 UTC 103.232.160.70

--- part1/app.js
+++ part1/app.js
@@ -23,7 +23,7 @@ let db;
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
 
     // Now connect to the created database
@@ -31,7 +31,7 @@ let db;
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'testdb'
+      database: 'DogWalkService'
     });
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
