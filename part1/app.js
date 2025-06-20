cfa37501b33dff1f492e4771529f99cdaf955958
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-20 06:24:52 +0000

    [automatic commit] 2025-06-20 06:24:52 UTC 103.232.160.70

--- part1/app.js
+++ part1/app.js
@@ -39,10 +39,11 @@ let db;
 })();
 
 // Route to return books as JSON
-app.get('/', async (req, res) => {
+app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
-    res.json(books);
+      const [dogs] = await db.execute('SELECT
+        ``);
+    res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
   }
