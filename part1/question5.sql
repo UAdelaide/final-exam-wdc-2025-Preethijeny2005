012191f6a7d8b9c69b60f101f915884f1acccafa
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-20 00:41:41 +0000

    [automatic commit] 2025-06-20 00:41:41 UTC 115.131.91.233

--- part1/question5.sql
+++ part1/question5.sql
@@ -3,7 +3,7 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('jeny321', 'jeny@exa')
+('jeny321', 'jeny@example.com', 'hashed321', 'walker')
 
 
 
