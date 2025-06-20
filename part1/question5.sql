74309ea63526ebc94508969e2ddabfac6e06b303
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-20 00:43:23 +0000

    [automatic commit] 2025-06-20 00:43:23 UTC 115.131.91.233

--- part1/question5.sql
+++ part1/question5.sql
@@ -4,11 +4,9 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('jeny321', 'jeny@example.com', 'hashed321', 'walker'),
-('sibin227', 'sibin@example.com', 'hashed227', 'owner')
-
-
+('sibin227', 'sibin@example.com', 'hashed227', 'owner');
 
 -- Insert: Five Dogs:
-
+INSERT INTO 
 
 -- Insert: Five walk request:
