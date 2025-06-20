e7229335579ba4fccf0ba8c078b3bea9ca57a339
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-20 08:27:41 +0000

    [automatic commit] 2025-06-20 08:27:41 UTC 103.232.160.70

--- part2/app.js
+++ part2/app.js
@@ -13,12 +13,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(session({
-  secret: 'secret_key',
+  secret: 'secretKey',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
-
 
 
 // Routes
