a8dbc3d09bb22d69f553936c2276f7c106b9a5ad
Preethi Jeny <Preethijeny2005@gmail.com>
2025-06-21 04:45:30 +0000

    [automatic commit] 2025-06-21 04:45:30 UTC 115.131.91.233

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -32,7 +32,10 @@ router.post('/login', async (req, res) => {
 
   try {
     const [rows] = await db.query(`
-      SELECT user_id`)
+      SELECT user_id, username, role FROM Users
+      WHERE username = ? AND password_hash = ?
+      
+      `)
   }
 })
 
